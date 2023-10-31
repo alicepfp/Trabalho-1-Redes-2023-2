90fa060ac80274a8a482c403c4c29de5ff556aad
Gustavo Pessanha <103529588+gupessanha@users.noreply.github.com>
2023-10-31 16:31:33 -0300

    adding error treatment to handler

--- server.py
+++ server.py
@@ -1,51 +1,58 @@
 import socket
 import threading
-import os 
+import os
 import time
 
-
-PORTA = 10000
-SERVER = '192.168.15.9'
+PORTA = 9999
+SERVER = '172.22.16.222'
 ADDR = (SERVER, PORTA)
 HEADER = 64
 FORMAT = 'utf-8'
-
 DISCONNECT = ':D'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+# Lista para armazenar todas as conexões
+conexoes = []
 
 def handler(conn, addr):
-    print(f'[NOVA CONEXÃO]: {addr} se conectou!')
+    try:
+        print(f'[NOVA CONEXÃO]: {addr} se conectou!')
 
-    conectado = True
-    # Adiciona a nova conexão à lista
-    conexoes.append(conn)
-    
-    while conectado:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT:
-                conectado = False
-                print(f"Desconctando: {addr}")
-                continue
+        conectado = True
+        # Adiciona a nova conexão à lista
+        conexoes.append(conn)
+        
+        while conectado:
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == DISCONNECT:
+                    conectado = False
+                    print(f"Desconctando: {addr}")
+                    continue
 
-            print(f'[{time.ctime()}][{addr}]: {msg}')
-            
-            # Envia a mensagem para todos os clientes
-            for conexao in conexoes:
-                conexao.send(f'[{time.ctime()}][{addr}]: {msg}'.encode(FORMAT))
+                print(f'[{time.ctime()}][{addr}]: {msg}')
+                
+                # Envia a mensagem para todos os clientes
+                for conexao in conexoes:
+                    conexao.send(f'[{time.ctime()}][{addr}]: {msg}'.encode(FORMAT))
 
-    # Remove a conexão da lista após desconectar
-    conexoes.remove(conn)
-    conn.close()
+    except Exception as e:
+        print(f"Erro: {e}")
+    finally:
+        # Remove a conexão da lista após desconectar
+        if conn in conexoes:
+            conexoes.remove(conn)
+        conn.close()
+
 
 def start():
     server.listen()
     print(f'O servidor está no endereço {SERVER}')
+    
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handler, args=(conn, addr))
