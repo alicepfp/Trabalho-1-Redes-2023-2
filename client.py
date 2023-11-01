6165d4be64623f3c38cce090dbadfd4902ffd4ee
alicepfp <alicepfp@gmail.com>
2023-11-01 17:24:47 -0300

    Refactor: remoção do envio de comprimento da mensagem

--- client.py
+++ client.py
@@ -12,10 +12,6 @@ client.connect(ADDR)
 
 def send(msg):
     message = msg.encode(FORMAT)
-    msg_len = len(message)
-    send_len = str(msg_len).encode(FORMAT)
-    send_len += b' ' * (HEADER - len(send_len))
-    client.send(send_len)
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
     
